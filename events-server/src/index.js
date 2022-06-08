b9979f14d27d40f752037aade72385d987519700
Daniela Gavidia <daniela@miro.com>
2022-06-08 16:28:01 +0200

    Add cursor:move event

--- events-server/src/index.js
+++ events-server/src/index.js
@@ -10,16 +10,25 @@ async function init() {
     console.log(event.data);
   });
 
+
   miro.board.ui.on('item:create', async (event) => {
       console.log(event.item)
       const payload = JSON.stringify({ item_created: event.item })
       ws.send(payload)
   });
+
   miro.board.ui.on('item:transform', async (event) => {
       console.log(event.items[0])
       const payload = JSON.stringify({ item_transformed: event.items })
       ws.send(payload)
   });
+
+  miro.board.ui.on('cursor:move', async (event) => {
+      console.log(event.x, ',', event.y)
+      const payload = JSON.stringify({ cursor_moved: event })
+      ws.send(payload)
+  });
+
 }
 
 init();
